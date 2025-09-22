73b85ba778d6c2ede25ba95df66bbfae7c809792
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2025-09-22 17:16:20 +0300

    changed listsum.py

--- Kurssit/Datarakenteet-Ja-Algoritmit/Viikko2/listsum.py
+++ Kurssit/Datarakenteet-Ja-Algoritmit/Viikko2/listsum.py
@@ -1,2 +1,2 @@
-find_sums = lambda ns, s: [(ss:=sum(ns[:s]))]+[(ss := ss+ns[i]-ns[i-s]) for i in range(s,len(ns))]
+find_sums=lambda ns,s:[(ss:=sum(ns[:s]))]+[(ss:=ss+ns[i]-ns[i-s])for i in range(s,len(ns))]
 
