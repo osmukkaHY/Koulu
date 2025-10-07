0df185818806d4aefc261bfa785e48b5c611c75b
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2025-10-07 12:31:33 +0300

    Continued permutation.py

--- Kurssit/Datarakenteet-Ja-Algoritmit/Viikko6/permutation.py
+++ Kurssit/Datarakenteet-Ja-Algoritmit/Viikko6/permutation.py
@@ -1,11 +1,21 @@
 class PermutationTracker:
     def __init__(self):
-        
+        self._numbers_set       = set()
+        self._numbers           = []
+        self._is_permutation    = True
 
     def append(self, number):
+        if number in self._numbers_set:
+            self._is_permutation = False
+            return
 
+        self._numbers += [0 for x in range(number-len(self._numbers))]
+        self._numbers[number-1] = number
+        self._numbers_set.add(number)
+        self._is_permutation = True if 0 not in self._numbers else False
 
     def check(self):
+        return self._is_permutation
 
 
 if __name__ == "__main__":
